fcb25b2137da7a5e67aa264386c669d2c0af5716
k44rme <romashkaplay1@gmail.com>
2026-05-25 09:37:54 +0300

    A little changes and poorly android support

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,12 +2,34 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #![allow(warnings)]
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
+
 mod music;
 mod config;
 
-use std::{fs, path::PathBuf};
+use std::{fs, path::PathBuf, process};
 use dunce;
 use serde_json;
+
+use toml_edit::DocumentMut;
+
+#[tauri::command]
+fn edit_config(new_music_path: &str) -> Result<(), String> {
+
+    // Read existing file
+    let contents = fs::read_to_string("../musicore.config.toml").unwrap_or_else(|err| {
+      println!("Error while finding the config.toml file. Error: ");
+      return err.to_string();
+    });
+    let mut doc = contents.parse::<DocumentMut>().unwrap();
+    
+    // Edit the music_path value
+    doc["music_path"] = toml_edit::value(new_music_path);
+    
+    // Write back (preserves comments and formatting)
+    fs::write("../musicore.config.toml", doc.to_string());
+    
+    Ok(())
+}
 
 #[tauri::command]
 async fn read_file(path: String) -> Result<Vec<u8>, String> {
@@ -30,7 +52,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_fs::init())
-        .invoke_handler(tauri::generate_handler![music::get_music_files, read_file, read_config])
+        .invoke_handler(tauri::generate_handler![music::get_music_files, read_file, read_config, edit_config])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/Music.tsx
+++ src/components/Music.tsx
@@ -9,6 +9,7 @@ function Music() {
   const [music, setMusic] = useState<MusicFile[]>([]);
   const [config, setConfig] = useState<Config>();
   const [ready, setReady] = useState(false);
+  const [path, setPath] = useState("");
 
   useEffect(() => {
     const cancelled = false
@@ -31,37 +32,70 @@ function Music() {
 
     const loadMusic = async () => {
       try {
-        const result = await invoke<string>("get_music_files", { 
-          musicPath: config?.music_path,
-        });
-        setMusic(JSON.parse(result));
+        if (config.music_path != "") {
+          const result = await invoke<string>("get_music_files", { 
+            musicPath: config?.music_path,
+          });
+          setMusic(JSON.parse(result));
+        }
       } catch (err) {
         console.error("Failed to load music:", err);
       }
     };
-  
+    
     loadMusic();
   }, [ready, config])
+  
+  if (config?.music_path == "") {
+    return (
+      <div style={{ position: "absolute", left: "50%", transform: "translateX(-50%)", top: "300px" }}>
+        Путь до вашей папки с музыкой?
+        <form>
+          <input type="text" name="music" id="set-music-path" value={path} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setPath(e.target.value)
+          }} />
+          <button type="submit" onClick={(e) => {
+            e.preventDefault()
+            const newMusicPath = async () => {
+              try {
+                invoke("edit_config", {
+                  newMusicPath: path
+                })
+                window.location.reload()
+                
+              } catch (error) {
+                console.error(error)
+              }
+            }
 
-  return (
-    <div className="song-list">
-      {music.map((file: MusicFile, index) => {
-        let song = "song song-"+index+1;
-        return(
-          <Link to={`/play/${file.id}`} key={file.file_name} state={file} className="song-link">
-            <div className={song} key={index}>
-                <img className="song-cover" src={"data:image/png;base64, "+file.image} alt="" />
-                <div className="song-info">
-                  <h3 className="song-title">{file.title}</h3>
-                  <p className="song-artist" >{file.artist}</p>
+            newMusicPath()
+          }}>OK</button>
+        </form>
+      </div>
+    )
+  } else {
+    return (
+      <div className="song-list">
+        {
+          music.map((file: MusicFile, index) => {
+            let song = "song song-"+index+1;
+            return(
+              <Link to={`/play/${file.id}`} key={file.file_name} state={file} className="song-link">
+                <div className={song} key={index}>
+                    <img className="song-cover" src={"data:image/png;base64, "+file.image} alt="" />
+                    <div className="song-info">
+                      <h3 className="song-title">{file.title}</h3>
+                      <p className="song-artist" >{file.artist}</p>
+                    </div>
+                      <span className="song-duration">{file.duration}</span>
                 </div>
-                  <span className="song-duration">{file.duration}</span>
-            </div>
-          </Link>
-        )
-      })}
-    </div>
-  );
+              </Link>
+            )
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default Music;
